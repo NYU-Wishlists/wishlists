4512f8c1ffc47121563e683d7256680b6e5bfd4f
JiayuanHu <jiayuan.hu@hotmail.com>
2018-11-17 23:13:43 -0500

    Refactor /wishlists

--- app/service.py
+++ app/service.py
@@ -1,67 +1,86 @@
 """
 Wishlist services
 
-Paths
+Paths:
 -----
-GET  /wishlists - Retrieves a list of wishlists from the database
-GET  /wishlists{id} - Retrirves a Wishlist with a specific id
+GET /healthcheck -- Check heart beat
+GET  /wishlists/ - Retrieves a list of wishlists from the database
+GET  /wishlists/{id}/items - Retrieves a Wishlist with a specific id
+GET /wishlists?wishlist_user="username" - Retrieves the list of wishlists for a user
 POST /wishlists - Creates a Wishlist in the datbase from the posted database
 PUT  /wishlists/{id} - Updates a Wishlist in the database fom the posted database
-DELETE /wishlists{id} - Removes a Wishlist from the database that matches the id
+DELETE /wishlists/{id} - Removes a Wishlist from the database that matches the id
+DELETE /wishlists/{wishlist_name} - Removes all wishlists that match a name
+DELETE /wishlists/{user_name}/delete_all - Removes all wishlists of a user
 """
 
-import os
 import sys
 import logging
-from flask import Response, jsonify, request, json, url_for, make_response
+from flask import jsonify, request, json, url_for, make_response, abort
+from flask_api import status    # HTTP Status Codes
+from flask_restplus import Api, Resource, fields
+from werkzeug.exceptions import NotFound
+from app.models import Pet, DataValidationError, DatabaseConnectionError
 from . import app
-from models import Wishlist,Wishlist_entry, DataValidationError
-
-# Pull options from environment
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
-PORT = os.getenv('PORT', '5000')
-
-# Status Codes
-HTTP_200_OK = 200
-HTTP_201_CREATED = 201
-HTTP_204_NO_CONTENT = 204
-HTTP_400_BAD_REQUEST = 400
-HTTP_404_NOT_FOUND = 404
-HTTP_409_CONFLICT = 409
 
 ######################################################################
-# Error Handlers
+# Configure Swagger before initilaizing it
 ######################################################################
-@app.errorhandler(DataValidationError)
+api = Api(app,
+          version='1.0.0',
+          title='Wishlist REST API Service',
+          description='This is a Wishlist store server.',
+          doc='/apidocs/'
+          # prefix='/api'
+         )
+
+
+# This namespace is the start of the path i.e., /pets
+ns = api.namespace('wishlists', description='Wishlist operations')
+
+# Define the model so that the docs reflect what can be sent
+wishlist_model = api.model('Wishlist', {
+    'id': fields.Integer(readOnly=True,
+                         description='The unique id assigned internally by service'),
+    'name': fields.String(required=True,
+                          description='The name of the Wishlist'),
+    'user': fields.String(required=True,
+                              description='The owner of the Wishlist'),
+    'entries': fields.Boolean(required=True,
+                                description='The items of the Wishlist')
+})
+
+
+######################################################################
+# Special Error Handlers
+######################################################################
+@api.errorhandler(DataValidationError)
 def request_validation_error(error):
-    """ Handles all data validation issues from the model """
-    return bad_request(error)
+    """ Handles Value Errors from bad data """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return {'status':400, 'error': 'Bad Request', 'message': message}, 400
 
-@app.errorhandler(400)
-def bad_request(error):
-    """ Handles requests that have bad or malformed data """
-    return jsonify(status=400, error='Bad Request', message=error.message), 400
-
-@app.errorhandler(404)
-def not_found(error):
-    """ Handles wishlists that cannot be found """
-    return jsonify(status=404, error='Not Found', message=error.message), 404
-
-@app.errorhandler(405)
-def method_not_supported(error):
-    """ Handles bad method calls """
-    return jsonify(status=405, error='Method not Allowed',
-                   message='Your request method is not supported.' \
-                   ' Check your HTTP method and try again.'), 405
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    """ Handles catostrophic errors """
-    return jsonify(status=500, error='Internal Server Error', message=error.message), 500
+@api.errorhandler(DatabaseConnectionError)
+def database_connection_error(error):
+    """ Handles Database Errors from connection attempts """
+    message = error.message or str(error)
+    app.logger.critical(message)
+    return {'status':500, 'error': 'Server Error', 'message': message}, 500
 
 
 ######################################################################
-# GET INDEX
+# GET HEALTH CHECK
+######################################################################
+@app.route('/healthcheck')
+def healthcheck():
+    """ Let them know our heart is still beating """
+    return make_response(jsonify(status=200, message='Healthy'), status.HTTP_200_OK)
+
+
+
+######################################################################
+# GET INDEX 
 ######################################################################
 @app.route('/')
 def index():
@@ -70,23 +89,6 @@ def index():
 					version='1.0',
 				   url=url_for('list_wishlists', _external=True)), HTTP_200_OK
 
-
-######################################################################
-# LIST ALL WISHLISTS
-######################################################################
-@app.route('/wishlists', methods=['GET'])
-def list_wishlists():
-    """ Retrieves all the wishlists from the database """
-    app.logger.info('Listing wishlists')
-    wishlists = []
-    wishlist_user = request.args.get('wishlist_user')
-
-    if wishlist_user:
-        wishlists = Wishlist.find_by_user(wishlist_user)
-    else:
-        wishlists = Wishlist.all()
-
-    return jsonify([wishlist.serialize() for wishlist in wishlists]), HTTP_200_OK
 
 ######################################################################
 # Delete a wishlist
@@ -148,22 +150,6 @@ def update_wishlist(wishlist_id):
 
 
 
-######################################################################
-# ADD A NEW WISHLIST
-######################################################################
-@app.route('/wishlists',methods=['POST'])
-def create_wishlist():
-    """ Creates a Wishlist in the database from the posted database"""
-    app.logger.info('Creating a new wishlist')
-    payload = request.get_json()
-    wishlist = Wishlist()
-    wishlist.deserialize(payload)
-    wishlist.save()
-    message = wishlist.serialize()
-    response = make_response(jsonify(message), HTTP_201_CREATED)
-    response.headers['Location'] = url_for('get_wishlist', wishlist_id=wishlist.id, _external=True)
-    return response
-
 
 
 ######################################################################
@@ -177,6 +163,59 @@ def delete_user_wishlists(user_name):
     for wishlist in wishlists:
         wishlist.delete_wishlist()
     return make_response('', HTTP_204_NO_CONTENT)
+
+
+######################################################################
+#  PATH: /wishlists
+######################################################################
+@ns.route('/', strict_slashes=False)
+class WishlistCollection(Resource):
+    """ Handles all interactions with collections of Wishlists """
+    #------------------------------------------------------------------
+    # LIST ALL WISHLISTS
+    #------------------------------------------------------------------    
+    @ns.doc('list_wishlists')
+    @ns.param('wishlist_user','List Wishlists of a user')
+    @ns.marshal_with(wishlist_model)
+    def get(self):
+        """ Retrieves all the wishlists """
+        app.logger.info('Request to list wishlists')
+        wishlists = []
+        wishlist_user = request.args.get('wishlist_user')
+
+        if wishlist_user:
+            wishlists = Wishlist.find_by_user(wishlist_user)
+        else:
+            wishlists = Wishlist.all()
+
+        app.logger.info('[%s] Wishlists returned', len(wishlists))
+        results = [wishlist.serialize() for wishlist in wishlists]
+        return results, HTTP_200_OK
+
+    #------------------------------------------------------------------
+    # ADD A NEW WISHLIST
+    #------------------------------------------------------------------
+    @ns.doc('create_wishlists')
+    @ns.expect(wishlist_model)
+    @ns.response(400, 'The posted data was not valid')
+    @ns.response(201, 'Wishlist created successfully')
+    @ns.marshal_with(pet_model, code = 201)
+    def post(self):
+        """
+        Creates a Wishlist
+        This endpoint will create a Wishlist based the data in the body that is posted
+        """
+        app.logger.info('Request to Create a wishlist')
+        check_content_type('application/json')
+        wishlist = Wishlist()
+        app.logger.info('Payload = %s', api.payload)
+        wishlist.deserialize(api.payload)
+        wishlist.save()
+        app.logger.info('Wishlist with new id [%s] saved', wishlist.id)
+        location_url = api.url_for(WishlistResource, wishlist_id=wishlist.id, _external=True)
+        return wishlist.serialize(), status.HTTP_201_CREATED, {'Location':location_url}
+
+
 
 ######################################################################
 # Demo DATA
@@ -192,6 +231,14 @@ def create_demo_data():
 ######################################################################
 #   U T I L I T Y   F U N C T I O N S
 ######################################################################
+def check_content_type(content_type):
+    """ Checks that the media type is correct """
+    if request.headers['Content-Type'] == content_type:
+        return
+    app.logger.error('Invalid Content-Type: %s', request.headers['Content-Type'])
+    abort(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, 'Content-Type must be {}'.format(content_type))
+
+
 def initialize_logging(log_level=logging.INFO):
     """ Initialized the default logging to STDOUT """
     if not app.debug:
