6c7016aa15c5bb024bf9bb4e193f4ea980465505
minhlai <minh.lai30@gmail.com>
2018-12-11 22:20:23 +0000

    wait 30 secs

--- features/steps/wishlist_steps.py
+++ features/steps/wishlist_steps.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-WAIT_SECONDS = 10
+WAIT_SECONDS = 30
 BASE_URL = getenv('BASE_URL', 'http://localhost:5000/')
 # INDEX_URL = getenv('BASE_URL', 'http://localhost:5000/index')
 
